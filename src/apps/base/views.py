d141a91eeb6ee90fad8a323485e9a0f44abfd8f5
Jhonatan Eduardo <jhonatanepp@gmail.com>
2019-01-29 11:01:56 -0200

    New adjustment in BASE app

--- src/apps/base/views.py
+++ src/apps/base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from apps.employees.models import Employee
 
 
 @login_required
 def base(request):
-    return render(request, 'base/index.html')
+    data = {}
+    data['usuario'] = request.user
+    return render(request, 'base/index.html', data)
