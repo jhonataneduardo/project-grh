453ef2c00f48d83ecc0ea8e28f560cbc4c16dd92
Jhonatan Eduardo <jhonatanepp@gmail.com>
2019-02-06 20:39:14 -0200

    Adjustments in file of configuration

--- src/gestao_rh/settings.py
+++ src/gestao_rh/settings.py
@@ -99,6 +99,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
--- src/gestao_rh/urls.py
+++ src/gestao_rh/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('apps.base.urls')),
     path('employees/', include('apps.employees.urls')),
     path('departaments/', include('apps.departments.urls')),
+    path('documents/', include('apps.documents.urls')),
     path('company/', include('apps.companies.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
