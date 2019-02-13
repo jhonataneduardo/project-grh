487d5f250ec3a129f1cd0b5d03743b8756d4889d
Jhonatan Eduardo <jhonatanepp@gmail.com>
2019-02-13 14:19:00 -0200

    Add method UPDATA in REGISTER_HOURS app

--- src/apps/register_hours/urls.py
+++ src/apps/register_hours/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterHourList
+from .views import RegisterHourList, RegisterUpdate
 
 urlpatterns = [
     path('', RegisterHourList.as_view(), name='register_hours_list'),
-    # path('edit/<int:pk>', EmployeeUpdate.as_view(), name='employees_update'),
+    path('edit/<int:pk>', RegisterUpdate.as_view(), name='register_hours_update'),
     # path('delete/<int:pk>', EmployeeDelete.as_view(), name='employees_delete'),
     # path('new/', EmployeeCreate.as_view(), name='employees_create'),
 ]
--- src/apps/register_hours/views.py
+++ src/apps/register_hours/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView
 from .models import RegisterHour
 
 
 class RegisterHourList(ListView):
     model = RegisterHour
-    fields = ['justification', 'hours']
+    fields = ['justification', 'hours', 'employee']
 
     def get_queryset(self):
         company_current = self.request.user.employee.company
         return RegisterHour.objects.filter(employee__company=company_current)
+
+
+class RegisterUpdate(UpdateView):
+    model = RegisterHour
+    fields = ['justification', 'hours', 'employee']
