77cc2f5dd2cf04f5baa0caf740858777ab10df6e
Jhonatan Eduardo <jhonatanepp@gmail.com>
2019-02-20 16:21:46 -0300

    Add method for calculate total hours extras

--- src/apps/employees/models.py
+++ src/apps/employees/models.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from ..departments.models import Department
 from ..companies.models import Company
 
@@ -9,6 +10,11 @@ class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departments = models.ManyToManyField(Department)
     company = models.ForeignKey(Company, on_delete=models.PROTECT, null=True, blank=True)
+
+    @property
+    def total_hours(self):
+        total = self.registerhour_set.all().aggregate(Sum('hours'))['hours__sum']
+        return total
 
     def __str__(self):
         return self.name
