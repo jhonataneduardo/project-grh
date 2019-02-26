46259043b2ae4dbf1639bf683bcf0a1db633355f
Jhonatan Eduardo <jhonatanepp@gmail.com>
2019-02-26 10:58:36 -0300

    New adjustments

--- src/apps/employees/urls.py
+++ src/apps/employees/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import EmployeesList, EmployeeUpdate, EmployeeDelete, EmployeeCreate
+from .views import EmployeesList, EmployeeUpdate, EmployeeDelete, EmployeeCreate, pdf_reportlab, Pdf
 
 urlpatterns = [
     path('', EmployeesList.as_view(), name='employees_list'),
     path('edit/<int:pk>', EmployeeUpdate.as_view(), name='employees_update'),
     path('delete/<int:pk>', EmployeeDelete.as_view(), name='employees_delete'),
     path('new/', EmployeeCreate.as_view(), name='employees_create'),
+    path('pdf-reportlab/', pdf_reportlab, name='pdf_reportlab'),
+    path('relatorio-html/', Pdf.as_view(), name='relatorio_html'),
 ]
 
--- src/apps/employees/views.py
+++ src/apps/employees/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from .models import Employee
 
+# Used for lib reportab
+import io
+from reportlab.pdfgen import canvas
+
+# used for lib xhtml2pdf
+from django.template.loader import get_template
+import xhtml2pdf.pisa as pisa
 
 class EmployeesList(ListView):
     model = Employee
@@ -34,3 +41,56 @@ class EmployeeCreate(CreateView):
         obj.company = self.request.user.employee.company
         obj.user = User.objects.create(username=username[0])
         return super(EmployeeCreate, self).form_valid(form)
+
+
+def pdf_reportlab(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="pdfteste.pdf"'
+
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(10, 810, "Hello, Word!")
+
+    palavras = ['palavra1', 'palavra2', 'palavra2']
+
+    y = 790
+    for palavra in palavras:
+        p.drawString(10, y, palavra)
+        y -= 40
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
+
+
+class Render:
+
+    @staticmethod
+    def render(path: str, params: dict, filename: str):
+        template = get_template(path)
+        html = template.render(params)
+        response = io.BytesIO()
+        pdf = pisa.pisaDocument(io.BytesIO(html.encode("UTF-8")), response)
+        if not pdf.err:
+            response = HttpResponse(response.getvalue(), content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename=%s.pdf' % filename
+            return response
+        else:
+            return HttpResponse("Error Redering PDF", status=500)
+
+
+class Pdf(View):
+
+    def get(self, request):
+        params = {
+            'today': 'Variavel teste today',
+            'sales': 'Variavel teste sales',
+            'request': request,
+        }
+        return Render.render('employees/relatorio.html', params, 'testenovo')
